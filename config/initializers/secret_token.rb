fbe9bb720a65010152f0600e396867a7224f3ae1
unknown <Fran@Fran-PC.(none)>
2013-08-14 20:33:41 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BeerApp::Application.config.secret_key_base = '3997dffbc455bd72d3e37e60e8ecade92d46c7acce3ccf070a0acdf485100be3d04a9752954ecce34d795c3d9709e558d81fdc01d20884a08549c1e2e4f0aceb'
